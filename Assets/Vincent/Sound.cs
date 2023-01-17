7adfc86791b3432f3f26d03a1682d902b37cd57e
Darwei1 <vincent.fajersson@elev.ga.lbs.se>
2023-01-17 14:21:05 +0100

    audio

---
+++ Assets/Vincent/Sound.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class Sound : MonoBehaviour
+{
+
+    public string name;
+    public AudioClip clip;
+
+}
